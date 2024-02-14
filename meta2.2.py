4ae2422cbe9b2f5fb7279837519e410f177e87d1
ZairaPinon <159564650+ZairaPinon@users.noreply.github.com>
2024-02-14 11:24:46 -0800

    Add files via upload

---
+++ meta2.2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 10:23:08 2024
+
+@author: zairapinon
+"""
+
+matriz=[[0,0,0], [0,0,0]]
+print("Dame el elemento[0][0]")
+matriz[0][0]=int(input( matriz[0][0]))
+print(matriz)
+print("Dame el elemento[0][1]")
+matriz[0][1]=int(input( matriz[0][1]))
+print(matriz)
+print("Dame el elemento[0][2]")
+matriz[0][2]=int(input( matriz[0][2]))
+print(matriz)
+print("Dame el elemento[1][0]")
+matriz[1][0]=int(input( matriz[1][0]))
+print(matriz)
+print("Dame el elemento[1][0]")
+matriz[1][1]=int(input( matriz[1][1]))
+print(matriz)
+print("Dame el elemento[1][0]")
+matriz[1][2]=int(input( matriz[1][2]))
+print(matriz)
+print("la matriz es:")
+print(matriz)
